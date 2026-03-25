518f6708133cb9ba9a653b36bd343dc3d9b3a13b
Diego11304 <diego.arevalo.cbtis110@gmail.com>
2026-03-25 10:56:21 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+//Imports
+import 'package:flutter/material.dart';
+import 'class/splashscreen.dart';
+//main
+void main(){
+  runApp(AdopcionesApp());
+}
+//clase adopciones app
+class AdopcionesApp extends StatelessWidget{
+  AdopcionesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Adopta',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0x009acd32)),
+        fontFamily: 'Roboto',
+      ),
+      home: SplashScreen(),
+    );
+  }
+
+}
---
+++ lib/model/mascota.dart
@@ -0,0 +1,15 @@
+class Mascota {
+  final String nombre;
+  final String tipo;
+  final int edad;
+  final String imagen;
+  final String descripcion;
+
+  Mascota({
+    required this.nombre,
+    required this.tipo,
+    required this.edad,
+    required this.imagen,
+    required this.descripcion,
+  });
+}
